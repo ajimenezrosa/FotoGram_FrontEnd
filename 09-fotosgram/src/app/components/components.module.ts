d2b6b9013ead696e8e91d7a1ae960a68b72ed79e
Alejandro <ja.jimenezrosa@gmail.com>
2020-02-21 08:26:24 -0400

    Mostrar las Coordenadas del Map

--- 09-fotosgram/src/app/components/components.module.ts
+++ 09-fotosgram/src/app/components/components.module.ts
@@ -5,6 +5,7 @@ import { PostComponent } from './post/post.component';
 import { IonicModule } from '@ionic/angular';
 import { PipesModule } from '../pipes/pipes.module';
 import { AvatarSelectorComponent } from './avatar-selector/avatar-selector.component';
+import { MapaComponent } from './mapa/mapa.component';
 
 
 
@@ -12,7 +13,8 @@ import { AvatarSelectorComponent } from './avatar-selector/avatar-selector.compo
   declarations: [
     PostsComponent,
     PostComponent,
-    AvatarSelectorComponent
+    AvatarSelectorComponent,
+    MapaComponent
   ],
   exports: [
     PostsComponent,
